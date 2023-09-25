509c08ccb5d2e0002efb42a715d92ddc30deab7c
Bartosz Slawianowski <bartosz.slawianowski@natzka.com>
2023-09-25 13:28:09 +0200

    Disable async for prefixes

--- config/ipam/config.go
+++ config/ipam/config.go
@@ -6,10 +6,12 @@ import "github.com/upbound/upjet/pkg/config"
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("netbox_prefix", func(r *config.Resource) {
 		r.ShortGroup = "ipam.netbox"
+		r.UseAsync = false
 	})
 
 	p.AddResourceConfigurator("netbox_available_prefix", func(r *config.Resource) {
 		r.ShortGroup = "ipam.netbox"
 		r.Kind = "AvailablePrefix"
+		r.UseAsync = false
 	})
 }
